6d50a829b53c7d30c659b510c6df5e8f5764d640
HifzaZaheer96 <Hifza.Zaheer@academytrainee.com>
2020-03-17 12:23:17 +0000

    updated testing.sh

--- script/testing.sh
+++ script/testing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
-coverage run -m pytest test/testing.py
-coverage report
+coverage run -m pytest ./test/testing.py
+coverage report -m
