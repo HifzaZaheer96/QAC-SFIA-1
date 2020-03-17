01e71eef4458f6edc8c8ba0b841fcffa084f8b6f
HifzaZaheer96 <Hifza.Zaheer@academytrainee.com>
2020-03-17 11:19:47 +0000

    updated installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -6,6 +6,12 @@ pip3 install flask
 
 pip3 install flask_mysqldb
 
+pip3 install pytest
+
+pip3 install urllib3
+
+pip3 install coverage
+
 source ~/bashrc
 
 python3 /var/lib/jenkins/workspace/"Pipeline 1"/app.py
