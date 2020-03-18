397694ab563059fc10ee4b8d594359de7e56d0e0
HifzaZaheer96 <Hifza.Zaheer@academytrainee.com>
2020-03-18 16:32:05 +0000

    added bashrc

--- script/testing.sh
+++ script/testing.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+source  ~/bashrc
 source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
 coverage run -m pytest ./test/testing.py
 coverage report
+
+# source  ~/bashrc
+# /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage run -m pytest /var/lib/jenkins/workspace/"Pipeline 1"/test/testing.py
+# /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage report 
