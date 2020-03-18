24dd10f4b77be15cbac1fd5b12ea0ee9813333e9
HifzaZaheer96 <Hifza.Zaheer@academytrainee.com>
2020-03-18 16:36:02 +0000

    testing config

--- script/make_service.sh
+++ script/make_service.sh
@@ -6,8 +6,3 @@ sudo systemctl enable flask.service
 sudo systemctl start flask.service
 
 
-sleep 10
-
-source  ~/bashrc
-/var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage run -m pytest /var/lib/jenkins/workspace/"Pipeline 1"/test/testing.py
-/var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage report 
--- script/testing.sh
+++ script/testing.sh
@@ -3,7 +3,3 @@ source  ~/bashrc
 source /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/activate
 coverage run -m pytest ./test/testing.py
 coverage report
-
-# source  ~/bashrc
-# /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage run -m pytest /var/lib/jenkins/workspace/"Pipeline 1"/test/testing.py
-# /var/lib/jenkins/workspace/"Pipeline 1"/venv/bin/coverage report 
